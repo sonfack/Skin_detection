4e2454c7e88f6552c49d4b42b9465518ec4fe8f2
Sonfack Serge <sss.sonfack@gmail.com>
2018-11-09 23:04:48 +0700

    Use of skin probability and non skin probability appearance

--- src/commons/SkinDetection.py
+++ src/commons/SkinDetection.py
@@ -45,3 +45,42 @@ class SkinDetection:
                     result = cv2.merge([R, G, B])
                     cv2.imwrite(str(count)+".jpg", result)
         print(" fin detection ")
+
+
+    def detectSkinCompareProbability(self, theta=0.0001):
+        x, y, z = self.imgMatrix.shape
+        emptyImage = np.zeros((x, y, 3))
+        R, G, B = cv2.split(emptyImage)
+        Lab = cv2.cvtColor(self.imgMatrix, cv2.COLOR_BGR2Lab)
+        l, a, b = cv2.split(Lab)
+        count = 0
+        skinProbability = 0
+        nonSkinProbability = 0
+        for i in range(x):
+            for j in range(y):
+                ca = a[i][j]
+                cb = b[i][j]
+                fileObject = open(self.skinModel, "r+")
+                count = count + 1
+                for line in fileObject.readlines():
+                    newline = line.split()
+                    if str(newline[0]) == str(ca) and str(newline[1]) == str(cb):
+                        skinProbability = newline[2]
+                fileObject.close()
+
+                fileObject = open(self.nonSkinModel, "r+")
+                count = count + 1
+                for line in fileObject.readlines():
+                    newline = line.split()
+                    if str(newline[0]) == str(ca) and str(newline[1]) == str(cb):
+                        nonSkinProbability = newline[2]
+                fileObject.close()
+
+                if float(skinProbability) / float(nonSkinProbability) > theta:
+                    R[i][j] = 255
+                    G[i][j] = 255
+                    B[i][j] = 255
+                if count % 50000 == 0:
+                    result = cv2.merge([R, G, B])
+                    cv2.imwrite(str(count) + ".jpg", result)
+        print(" fin detection ")
